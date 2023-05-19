4f72ad66b015e516c1623d6e88e390c13b7bc85a
Thomas Chatzopoulos <chatzothomas@gmail.com>
2023-05-19 14:54:32 +0300

    add functionality: store Label Model probabilities

--- weak_supervision_enchancement/snorkel_labeling/snorkel_model.py
+++ weak_supervision_enchancement/snorkel_labeling/snorkel_model.py
@@ -22,16 +22,18 @@ from weak_supervision_enchancement.snorkel_labeling.snorkel_model_functions impo
     calcStatsForVoters,
     calcStatsForLFs,
     saveVoterPredictions,
+    saveProbs
 )
 from weak_supervision_enchancement.utils.utils import (
     createSummaryReport,
     saveResultsToJSON,
     createDir,
-    printTimestamp,
+    printTimestamp
 )
 
 settings = load(open("../../weak_supervision_enchancement/settings.yaml"), Loader=FullLoader)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = settings["TF_CPP_MIN_LOG_LEVEL"]
+# For reproducibility
 os.environ["PYTHONHASHSEED"] = settings["PYTHONHASHSEED"]
 
 for year in range(settings["firstYear"], settings["lastYear"] + 1, 1):
@@ -45,6 +47,8 @@ for year in range(settings["firstYear"], settings["lastYear"] + 1, 1):
     lf_test_results = {}
     X_voter_results = {}
     Y_voter_results = {}
+    X_probs = {}
+    Y_probs = {}
     selected_lfs = [0] * len(LFsToUse)
 
     filesPath = f"{settings['filesPath']}/Dataset_SI_old_{year}"
@@ -71,7 +75,7 @@ for year in range(settings["firstYear"], settings["lastYear"] + 1, 1):
         )
 
         # Apply Voters
-        X_pred_lm, Y_pred_lm = applyLabelModel(L_train, L_test, False)
+        X_pred_lm, Y_pred_lm, X_prob_lm, Y_prob_lm = applyLabelModel(L_train, L_test, False, settings["return_probs"])
         X_pred_maj, Y_pred_maj = applyMajorityVoter(L_train, L_test)
         X_pred_min, Y_pred_min = applyMinorityVoter(L_train, L_test)
 
@@ -82,6 +86,11 @@ for year in range(settings["firstYear"], settings["lastYear"] + 1, 1):
             Y_voter_results, Y_pred_lm, Y_pred_maj, Y_pred_min
         )
 
+        X_probs[f"{d}_0"] = X_prob_lm[:, 0]
+        X_probs[f"{d}_1"] = X_prob_lm[:, 1]
+        Y_probs[f"{d}_0"] = Y_prob_lm[:, 0]
+        Y_probs[f"{d}_1"] = Y_prob_lm[:, 1]
+
         if settings["findKBestLFs"]:
             Data, selected_lfs = selectKBestLFs(
                 L_test,
@@ -90,9 +99,9 @@ for year in range(settings["firstYear"], settings["lastYear"] + 1, 1):
                 selected_lfs
             )
 
-    saveVoterPredictions(
-        X_voter_results, trainData, descrIDs, workingPath, "train", year
-    )
+    saveProbs(workingPath, trainData["pmid"], X_probs, testData["pmid"], Y_probs)
+
+    saveVoterPredictions(X_voter_results, trainData, descrIDs, workingPath, "train", year)
     saveVoterPredictions(Y_voter_results, testData, descrIDs, workingPath, "test", year)
 
     saveResultsToJSON(X_voter_results, workingPath, "voters_X_results", year)
